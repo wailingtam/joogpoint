b4a9fdb5e1e1a95af4efb6d1e0f3a3360f928887
Wai Ling Tam <wai.ling.tam@est.fib.upc.edu>
2016-06-04 20:07:37 +0200

    Database config

--- joogpoint/settings.py
+++ joogpoint/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 
 
 # Access to environment variables
@@ -111,6 +113,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
