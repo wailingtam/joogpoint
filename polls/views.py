6c916905d3849531b5be202010a47d84b6e6d76b
Wai Ling Tam <wai.ling.tam@est.fib.upc.edu>
2016-09-19 02:45:16 +0200

    Use requests instead of urllib

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions, viewsets
 from rest_framework import response, status
 from rest_framework.decorators import detail_route, api_view, permission_classes
-from urllib import request
+import requests
 import json
 from django.http import HttpResponse
 import spotipy
@@ -375,9 +375,8 @@ def song_search(request):
 def get_most_recent_track(playlist_pk):
     playlist = Playlist.objects.get(pk=playlist_pk)
     username = playlist.establishment.lastfm_username
-    results = request.urlopen("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username +
-                                     "&api_key=" + LASTFM_API_KEY + "&format=json&limit=1").read().decode("utf-8")
-    dic = json.loads(results)
+    dic = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username +
+                                     "&api_key=" + LASTFM_API_KEY + "&format=json&limit=1").json()
     data = {
         'now_playing': True,
         'spotify_uri': ""
